ae3812c645d9fa203ae8233693a367e8113a8b47
Mariano Toscano <marianotoscano@outlook.com>
2019-07-09 19:43:46 +0200

    Updates

---
+++ precache-manifest.00a170525f786d303a9b85ee726dac1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f48ead4fa9ffe24cfcd63d074fd6237",
+    "url": "/myswapiwebapp/index.html"
+  },
+  {
+    "revision": "f851514350d5601dd718",
+    "url": "/myswapiwebapp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "f27d7c374433bcfe7eac",
+    "url": "/myswapiwebapp/static/css/main.9a40202a.chunk.css"
+  },
+  {
+    "revision": "f851514350d5601dd718",
+    "url": "/myswapiwebapp/static/js/2.f7527fa3.chunk.js"
+  },
+  {
+    "revision": "f27d7c374433bcfe7eac",
+    "url": "/myswapiwebapp/static/js/main.96e01684.chunk.js"
+  },
+  {
+    "revision": "9f4d769d98be711435ab",
+    "url": "/myswapiwebapp/static/js/runtime~main.7330d2af.js"
+  },
+  {
+    "revision": "751a045f9fd91b7200a77b454f53f891",
+    "url": "/myswapiwebapp/static/media/star-wars-logo.751a045f.svg"
+  }
+]);
